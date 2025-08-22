cbdfb78ffff74a8e1a7e4559a32035bfbc57466b
Jason Meinzer <jasonmeinzer@jasonmeinzer.com>
2025-08-21 17:38:44 -0700

    Create Web3Provider.tsx

---
+++ src/components/Web3Provider.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { WagmiProvider, createConfig, http } from "wagmi";
+import { base } from "wagmi/chains";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { farcasterMiniApp as miniAppConnector } from '@farcaster/miniapp-wagmi-connector';
+
+export const config = createConfig({
+  chains: [base],
+  transports: {
+    [base.id]: http(),
+  },
+  connectors: [
+    miniAppConnector()
+  ]
+});
+
+const queryClient = new QueryClient();
+
+interface Web3ProviderProps {
+  children: React.ReactNode;
+}
+
+export const Web3Provider: React.FC<Web3ProviderProps> = ({ children }) => {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        {children}
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+};
