a61e6d17b593179500d0da684028021c6519f40e
Jason Meinzer <jasonmeinzer@jasonmeinzer.com>
2025-08-21 17:34:44 -0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['i.imgur.com', 'imagedelivery.net'],
+  },
+  experimental: {
+    appDir: true,
+  },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    return config;
+  },
+}
+
+module.exports = nextConfig
